ec86c598a1a6c0f29c416ce2d349fa507a171230
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-03 13:56:48 -0400

    shadcn card & currency converter component

---
+++ client/src/components/Converter.tsx
@@ -0,0 +1,24 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
+
+const Converter = () => {
+    return (
+        <Card className="max-width-300px">
+            <CardHeader className="border-b-2 p-2 m-2">
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p>Card Content</p>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Converter
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,8 +2,8 @@
 export default {
     darkMode: ["class"],
     content: [
-		'./pages/**/*.{html,js}',
-    	'./components/**/*.{html,js}',
+		'./index.html', 
+		'./src/**/*.{js,ts,jsx,tsx}'
 	],
   theme: {
   	extend: {
