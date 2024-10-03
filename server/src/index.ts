85c3e6a7e897b8f9db04cce99956b010f72f7f70
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-03 12:41:55 -0400

    setup routes and currency files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,28 @@
+import { serveStatic } from 'hono/bun'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 
 const app = new Hono()
+
 app.use('/*', cors())
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use('/data/*', serveStatic({root: './'}))
+
+app.get('/currencies', (c) => {
+  return c.redirect('/data/currencies.json')
 })
+
+app.get('/names', (c) => {
+  return c.redirect('/data/names.json')
+})
+
+const PORT = 8088
+
+const server = Bun.serve({
+  port: PORT,
+  fetch: app.fetch
+})
+
+console.log(`Server is running on port ${server.port}`);
 
 export default app
